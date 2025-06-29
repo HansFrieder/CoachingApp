92de7e52653cdd9796c65fa997cb3ff9fec00930
Frieder <frieder.hens@gmail.com>
2025-06-30 00:27:53 +0200

    "Login Methode"

--- coaching_app/settings.py
+++ coaching_app/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login and Logout settings
+LOGIN_REDIRECT_URL = '/' # Nach erfolgreichem Login hierhin
+LOGOUT_REDIRECT_URL = '/login/'# Nach Logout hierhin
--- coaching_app/urls.py
+++ coaching_app/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.mainpage, name="mainpage"),  # Mainpage
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), # Login
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'), # Logout
 ]
